7c80d9f26e8072a55557c13bfdc3bc1405602337
ked769 <conversations.ked769@gmail.com>
2025-06-27 13:53:23 +0530

    addded code anol i think it's done

--- main.py
+++ main.py
@@ -22,3 +22,33 @@ async def on_ready():
         print(e)    # oops, fucked up
 
 
+@tree.command(name="setmarks", description="Set BITSAT marks role", guild=discord.Object(id=GUILD_ID))
+@app_commands.describe(marks="Your BITSAT score (0–390)")
+async def set_marks(interaction: discord.Interaction, marks: int):
+    if marks < 0 or marks > 390:
+        await interaction.response.send_message("Stop lying, chutiye", ephemeral=True)
+        return
+
+    role_name = str(marks)
+    guild = interaction.guild
+    member = interaction.user
+
+    # making role
+    role = discord.utils.get(guild.roles, name=role_name)
+    if not role:
+        try:
+            role = await guild.create_role(name=role_name)
+        except discord.Forbidden:
+            await interaction.response.send_message("permission insufficient", ephemeral=True)
+            return
+
+    # in case you already got marks assigned
+    for r in member.roles:
+        if r.name.isdigit() and 0 <= int(r.name) <= 390:
+            await member.remove_roles(r) # get unmarked
+
+    # Assign the new role
+    await member.add_roles(role)
+    await interaction.response.send_message(f"You’ve been given `{marks}` flair! Now take lite", ephemeral=True)
+
+bot.run(TOKEN)
