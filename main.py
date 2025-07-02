d145551fc0c369df8cd8635699f3bf4583333e86
Ked769 <conversations.ked769@gmail.com>
2025-07-02 12:10:45 +0530

    Added senior nicks and resetmarks command

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 tree = bot.tree  # to do those cool /set commands
 
+d = dict()
+users = dict()
 
 @bot.event
 async def on_ready():
@@ -19,9 +21,19 @@ async def on_ready():
     try:
         synced = await tree.sync(guild=discord.Object(id=GUILD_ID))
         print(f"Synced {len(synced)} commands to guild.")
+        global d
+        f = open("seniors.txt")
+        for line in f:
+            uid, nick = line.split()
+            d[uid] = nick
+        f.close()
+        f = open("users.txt")
+        for line in f:
+            uid, nick = line.split()
+            users[uid] = nick
+        f.close()
     except Exception as e:
         print(e)    # oops, fucked up
-
 
 @tree.command(name="setmarks", description="Set BITSAT marks role", guild=discord.Object(id=GUILD_ID))
 @app_commands.describe(marks="Your BITSAT score (0–390)")
@@ -34,8 +46,30 @@ async def set_marks(interaction: discord.Interaction, marks: int):
     guild = interaction.guild
     member = interaction.user
 
-    #adding marks to side and stuff
+    if member.name in users and member.nick != users[member.name]:
+        users[member.name] = member.nick
+        f = open("users.txt", "a")
+        f.write(f"{member.name} {member.nick}\n")
+
+    if member.name in d:
+        await member.edit(nick=d[member.name])
+        await interaction.response.send_message(
+            f"✅ Senior nick alloted! Hope ya like it :)", ephemeral=True
+        )
+        return
+    elif member.name == "floatingreeds":
+        await interaction.response.send_message("sorry, I don't think you'd like any nickname I give you.", ephemeral=True)
+        return
+
+    # Save original nickname to users.txt if this is their first time using setmarks
     current_nick = member.nick if member.nick else member.name
+    if member.name not in users:
+        # Save to users.txt
+        with open("users.txt", "a") as f:
+            f.write(f"{member.name} {current_nick}\n")
+        d[member.name] = current_nick
+
+    #adding marks to side and stuff
     if current_nick[0] == "[" and "]" in current_nick[1:5]:
         current_nick = current_nick[current_nick.index("]")+1:]
 
@@ -53,6 +87,23 @@ async def set_marks(interaction: discord.Interaction, marks: int):
     await interaction.response.send_message(
         f"✅ Nickname updated to `{new_nick}`! Now take lite :)", ephemeral=True
     )
+
+@tree.command(name="resetnick", description="Reset to your original nickname", guild=discord.Object(id=GUILD_ID))
+async def reset_nick(interaction: discord.Interaction):
+    member = interaction.user
+    
+    if member.name not in users:
+        await interaction.response.send_message("You don't have an original nickname saved!", ephemeral=True)
+        return
+        
+    try:
+        await member.edit(nick=users[member.name])
+        await interaction.response.send_message(
+            f"✅ Nickname reset to your original: `{users[member.name]}`", ephemeral=True
+        )
+    except discord.Forbidden:
+        await interaction.response.send_message("I don't have permission to change your nickname 😕 contact ked769#9886", ephemeral=True)
+
 bot.run(TOKEN)
 
 #well i hope it works now
